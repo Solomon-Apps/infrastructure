f76becfa7f6608e3e6f02f02caae5fec92da1aab
Solomon Wakhungu <1solomonwakhungu@gmail.com>
2025-04-11 20:14:33 -0500

    fix: update env

--- talos-control-plane/variables.tf
+++ talos-control-plane/variables.tf
@@ -1,18 +1,3 @@
-variable "proxmox_api_url" {
-  description = "Proxmox API URL"
-  type        = string
-}
-
-variable "proxmox_api_token_id" {
-  description = "Proxmox API token ID"
-  type        = string
-}
-
-variable "proxmox_api_token_secret" {
-  description = "Proxmox API token secret"
-  type        = string
-}
-
 variable "proxmox_nodes" {
   description = "Proxmox node name where VMs will be created"
   type        = list(string)
