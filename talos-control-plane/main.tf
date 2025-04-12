743051674bf8157b57218a49bc532afd90c5aa36
Solomon Wakhungu <1solomonwakhungu@gmail.com>
2025-04-11 20:24:36 -0500

    fix: update on boot

--- talos-control-plane/main.tf
+++ talos-control-plane/main.tf
@@ -8,7 +8,6 @@ resource "proxmox_vm_qemu" "kube_nodes" {
   memory      = var.vm_memory
   cores       = var.vm_cores
   sockets     = 1
-  onboot      = true
   disk {
     type    = "scsi"
     storage = var.vm_storage
